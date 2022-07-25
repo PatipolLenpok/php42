db09c8198c6fa39a4da0b1c8ff5f8e99adc6a641
PatipolLenpok <65309010035@tatc.ac.th>
2022-07-25 17:00:02 +0700

    MiniProject

---
+++ Project.php
@@ -0,0 +1,58 @@
+<?php
+    if(isset($_POST['submit']))
+    {
+        $Teacher_id=$_POST['Teacher_id'];
+        $Teacher_password=$_POST['Teacher_password'];
+        echo $Teacher_id."<br>";
+        echo $Teacher_password;
+        $con=mysqli_connect('localhost','root','','miniproject') or die ("ไม่สามารถเชื่อมต่อ Database ได้");
+        /*if(!$con){
+            echo "ไม่สามารถเชื่อมต่อ Database ได้";
+        }else{
+            echo "เชื่อมต่อ Database สำเร็จ";
+        }*/
+        $sql= "SELECT * FROM teacher WHERE Teacher_id='$Teacher_id' and Teacher_password='$Teacher_password'";
+        //mysqli_query($sql,$con);
+        $result=$con->query($sql);
+        /*$row=mysqli_fetch_array($result);
+        echo $row['username'];
+        echo $row['password'];*/
+        $num=mysqli_num_rows($result);
+        if($num==0){
+            echo "<script>alert('username หรือ password ไม่ถูกต้อง')</script>";
+        }else{
+            header('location:admin/index.php');
+        }
+    }
+        
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body style="background:#f1f1f1">
+    <div class="container w-25" style="margin-top:300px;">
+        <div class="card">
+            <div class="card-header bg-success text-white">Login </div>
+            <div class="card-body ">
+                <form action="<?php $_SERVER['PHP_SELF']?>" method="POST">
+                    <div class="mb-3">
+                        <label for="" class="form-label">TeacherID</label>
+                        <input type="text" class="form-control" name="Teacher_id">
+                    </div>
+                    <div class="mb-3">
+                        <label for="" class="form-label">TeacherPassword</label>
+                        <input type="password" class="form-control" name="Teacher_password">
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary">Login</button>
+                </form>   
+            </div>
+        </div>
+    </div>
+</body>
+</html>
