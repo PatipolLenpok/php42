de8264d56e4d40cea42133907b5839e879037df2
PatipolLenpok <65309010035@tatc.ac.th>
2022-08-01 14:08:44 +0700

    te

--- admin/user.php
+++ admin/user.php
@@ -2,5 +2,33 @@
    include 'navbar.php';
    include '../connect.php';
    $sql="SELECT * FROM user";
-   $result=$con->query($sql);
+   $result=$con->query($sql);//$result-mysqli_query($con,$sql);
+   
 ?>
+<div class="container mt-5 w-75">
+   <a href="add_user.php" class="btn btn-primary mb-3">+เพิ่มข้อมูล</a>
+   <table class="table table-striped">
+      <tr class="bg-success">
+      <th class="text-white">ลำดับที่</th>
+      <th class="text-white">ชื่อ - นามสกุล</th>
+      <th class="text-white">username</th>
+      <th class="text-white">email</th>
+      <th class="text-white">การจัดการ</th>
+      </tr>
+      <?php
+         $count=1; 
+         while($row=mysqli_fetch_array($result))
+         {
+
+         
+      ?>
+      <tr>
+         <td><?php echo $count;$count++;?></td>
+         <td><?php echo $row['Name']?></td>
+         <td><?php echo $row['Username']?></td>
+         <td><?php echo $row['Email']?></td>
+         <td>แก้ไข/ลบ</td>
+      </tr>
+      <?php }?>
+   </table>
+</div>
