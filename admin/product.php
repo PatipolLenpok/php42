1570baa0b5b4b5753f20dee0703e418fbc072f5a
PatipolLenpok <65309010035@tatc.ac.th>
2022-08-22 14:06:43 +0700

    asd

--- admin/product.php
+++ admin/product.php
@@ -6,7 +6,26 @@
    
 ?>
 <div class="container mt-5 w-75">
-   <a href="add_product.php" class="btn btn-primary mb-3">+เพิ่มข้อมูล</a>
+   <div class="row">
+        <div class="col-4">
+            <a href="add_user.php" class="btn btn-primary mb-3">+เพิ่มข้อมูล</a>
+        </div>
+        <div class="col-8">
+            <form action="user_uploadcsv.php" method="POST" enctype="multipart/form-data">
+            <div class="row">
+            <div class="col-3">
+                    <label for="" class="">อัพโหลดโหลดไฟล์</label>
+                </div>
+                <div class="col-5">
+                    <input type="file" class="form-control" class="mb-3" name="csv_file">
+                </div>
+                <div class="col-4">
+                    <input type="submit" name="addm" class="btn btn-primary mb-3" value="+เพิ่มข้อมูลที่ละหลายคน">
+                </div>
+            </div>
+            </form>
+        </div>
+    </div>
    <table class="table table-striped">
       <tr class="bg-success">
       <th class="text-white">รหัสสินค้า</th>
---
+++ admin/product_uploadcsv.php
@@ -0,0 +1,17 @@
+<?php
+    include '../connect.php';
+    $filename=$_FILES['csv_file']['name'];
+    if(isset($filename)){
+        move_uploaded_file($_FILES['csv_file']['tmp_name'],'csv/'.$filename);
+        $file_csv=fopen('csv/'.$filename,"r");
+        while($row_csv=fgetcsv($file_csv,1000,",")){
+            $pro_name=$row_csv[1];
+            $pro_price=$row_csv[2];
+            $pro_qty=$row_csv[3];
+            $sql2="INSERT INTO product(pro_name,pro_price,pro_qty) VALUES('$pro_name','$pro_price','$pro_qty')";
+            $result2=$con->query($sql2);
+        }
+        fclose($file_csv);
+        header('location:product.php');
+    }
+?>
